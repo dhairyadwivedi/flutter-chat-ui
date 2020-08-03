ee58bd99c180963c1104a4aea100f6571ca96887
dhairyadwivedi <dhairiya96@gmail.com>
2020-08-04 01:14:24 +0530

    add appBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(fontSize: 30.0),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
